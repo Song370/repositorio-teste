a84bcf5f2b95524b4d984eec884a3c089dffea95
Adriano Song <adriano@adrianoSong.(none)>
2012-09-30 15:31:54 -0300

    primeiro arquivo adicionado

---
+++ workspace/ola/src/olaMundo.java
@@ -0,0 +1,13 @@
+
+public class olaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola git fdp");
+
+	}
+
+}
